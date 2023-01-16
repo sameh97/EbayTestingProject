a3b9b74299541c65ec8efff8b224234cc9832238
Sameh Hassoun <sameh7462@gmail.com>
2023-01-16 08:21:25 +0200

    added screenshot fuction

--- src/main/java/Utils/Constants.java
+++ src/main/java/Utils/Constants.java
@@ -6,6 +6,8 @@ public class Constants {
 	public static final String CHROME = "Chrome";
 	public static final String URL = "https://www.ebay.com";
 	public static final String BROWSER = "browser";
-	
+	public static final String PNG_EXTENSION = ".png";
+	public static final int SCREENSHOTS_NAME_LENGTH = 7;
+	public static final String SCREENSHOTS_FOLDER = "screenshots\\";
 
 }
