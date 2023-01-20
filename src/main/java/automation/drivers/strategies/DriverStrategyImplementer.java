ee2ea7793fff80c982588787ddc4cfca160304f5
sameh97 <75888057+sameh97@users.noreply.github.com>
2023-01-20 10:53:04 +0200

    code refactor

--- src/main/java/automation/drivers/strategies/DriverStrategyImplementer.java
+++ src/main/java/automation/drivers/strategies/DriverStrategyImplementer.java
@@ -2,18 +2,21 @@ package automation.drivers.strategies;
 
 import automation.Utils.Constants;
 import automation.Utils.Utils;
+import automation.drivers.strategies.Chrome;
+import automation.drivers.strategies.DriverStrategy;
 
 public class DriverStrategyImplementer {
-	public static DriverStrategy chooseStrategy(String strategy) {
-		if (!Utils.hasValue(strategy)) {
-			return null;
-		}
 
-		switch (strategy) {
-		case Constants.CHROME:
-			return new Chrome();
-		default:
-			return null;
-		}
-	}
+    public static DriverStrategy chooseStrategy(String strategy) {
+        if (!Utils.hasValue(strategy)) {
+            return null;
+        }
+
+        switch (strategy) {
+            case Constants.CHROME:
+                return new Chrome();
+            default:
+                return null;
+        }
+    }
 }
